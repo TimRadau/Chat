be03fe6b5e74b776cb7c12b7d3f809dc32f3542a
TimRadau <tim.radau@web.de>
2024-12-01 00:36:54 +0100

    test
    test to check if repo worls

--- main/server.py
+++ main/server.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, jsonify
 import requests
 
 
-
+#testestest
 app = Flask(__name__)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
---
+++ main/test.py
@@ -0,0 +1,2 @@
+#TESTESTESTEST
+a =2
